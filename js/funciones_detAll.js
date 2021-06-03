58b5732351735f41b5d9b7c429aa169f7bff09b6
Aziz863 <55482239+Aziz863@users.noreply.github.com>
2021-06-03 21:14:42 +0200

    arreglado problema del bucle en la búsqueda

--- js/funciones_detAll.js
+++ js/funciones_detAll.js
@@ -1,36 +1,45 @@
-addEventListener('load', getAll(5), false);
+// ejecutamos la función d erepetir al cargar la página
+addEventListener('load', repetir(6), false);
 
+// función que genera las busquedas el número de veces pasado por parámetro
+function repetir(veces) {
 
-function getAll(num) {
-    for (let i = 0; i < 7; i++) {
-        $.ajax({
-            url: "https://rickandmortyapi.com/api/character/" + num,
-            method: 'GET',
-            dataType: 'json',
-            contentType: 'application/x-www-form-urlencoded',
+    // variables para indicar la posición de la busqueda en la api y la posición del div donde lo enviamos
+    let busqueda = 25;
+    let posicion = 1;
 
-            success: function(data) {
-                console.log(data.name);
-                console.log(data.species);
-                console.log(data.gender);
-                console.log(data.origin.name);
-                console.log(data.image);
-                for (let index = 1; index < 7; index++) {
-
-                    $('#nombre' + index).html(data.name);
-                    $('#especie' + index).html(data.species);
-                    $('#sexo' + index).html(data.gender);
-                    $('#origen' + index).html(data.origin.name);
-                    $('#imagen' + index).html(data.image);
-                    num++;
-                }
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });
-
+    // lanzamos la busqueda e incrementamos los valores
+    for (let index = 1; index < veces + 1; index++) {
+        getAll(busqueda, posicion)
+        busqueda++;
+        posicion++;
     }
+}
 
+// función que genera las busquedas en la api 
+function getAll(busqueda, posicion) {
+
+    // busqueda del personaje en la posición 'busqueda' pasada por parámetro
+    $.ajax({
+        url: "https://rickandmortyapi.com/api/character/" + busqueda,
+        method: 'GET',
+        dataType: 'json',
+        contentType: 'application/x-www-form-urlencoded',
+
+        // enviamos cada respuesta seleccionada de la busqeda a un elemnto por id
+        success: function(data) {
+            $('#nombre' + posicion).html(data.name);
+            $('#especie' + posicion).html(data.species);
+            $('#sexo' + posicion).html(data.gender);
+            $('#origen' + posicion).html(data.origin.name);
+            $('#imagen' + posicion).html(data.image);
+
+        },
+
+        // en caso de no encontrar la busqueda, nos envía un error por consola
+        error: function(error) {
+            console.log(error);
+        }
+    });
 
 }
